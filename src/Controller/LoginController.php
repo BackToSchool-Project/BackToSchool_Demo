8bd068ac40f280307e8d424c7c5357bf4d2f4561
GB-LD <gbld@mailo.com>
2022-11-28 15:32:43 +0100

    creation login form & import twig form template

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,10 @@ class LoginController extends AbstractController
     #[Route('/login', name: 'app_login')]
     public function login(): Response
     {
-        return $this->render('login/login.html.twig');
+        $form = $this->createForm(LoginType::class);
+
+        return $this->render('login/login.html.twig', [
+            'formView' => $form->createView()
+        ]);
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,19 +6,29 @@ use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    protected $hasher;
+
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
+
         $faker = Factory::create('fr_FR');
 
         $userTest = new User();
+        $passwordHash = $this->hasher->hashPassword($userTest, "password");
         $userTest
             ->setLastName('Doe')
             ->setFirstName('John')
             ->setEmail('john.doe@gmail.com')
-            ->setPassword('password')
+            ->setPassword($passwordHash)
             ->setCity('Marseille')
             ->setPosteCode(13001);
 
@@ -27,11 +37,12 @@ class AppFixtures extends Fixture
         for ($u = 0 ; $u < 50; $u++) {
 
             $user = new User();
+            $passwordHash = $this->hasher->hashPassword($user ,$faker->password());
             $user
                 ->setFirstName($faker->firstName())
                 ->setLastName($faker->lastName())
                 ->setEmail($faker->email())
-                ->setPassword($faker->password())
+                ->setPassword($passwordHash)
                 ->setCity('Marseille')
                 ->setPosteCode(13001);
 
